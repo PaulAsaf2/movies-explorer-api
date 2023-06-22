6809a4161fbcfca721c993b9ca99cac8294b8423
Pavel Asafov <pavelaof@gmail.com>
2023-06-22 14:32:05 +0600

    изменение порта на 3500

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const handleErrors = require('./errors/handleErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { limiterOptions } = require('./config');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3500 } = process.env;
 const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 app.listen(PORT, () => {

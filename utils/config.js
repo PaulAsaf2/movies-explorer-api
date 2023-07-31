73a19705962bc2d5b9988245daeda66723b4f165
Pavel Asafov <pavelasaf@yandex.ru>
2023-07-31 16:58:55 +0600

    добавил в cors адрес фронтенда

--- utils/config.js
+++ utils/config.js
@@ -19,7 +19,7 @@ exports.mongodbPath = NODE_ENV === 'production'
   : 'mongodb://127.0.0.1:27017/bitfilmsdb';
 
 exports.corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://diploma.nomoreparties.sbs'],
   credentials: true,
   maxAge: 3600,
 };

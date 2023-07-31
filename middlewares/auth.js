3505530269137b6c248b3ac6709fcadeb2a9cd58
Pavel Asafov <pavelasaf@yandex.ru>
2023-07-31 20:46:53 +0600

    в auth проверка токена

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,7 +10,9 @@ const auth = async (req, res, next) => {
   let payload;
   try {
     const token = await req.cookies.jwt;
+    console.log(token);
     payload = await jwt.verify(token, JWT_SECRET || developmentKey);
+    console.log(payload);
     req.user = payload;
   } catch {
     return next(new UnauthorizedError(authorisationError));
